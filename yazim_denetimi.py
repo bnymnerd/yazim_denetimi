f0ed9d90e9f2d74a015bb895414ad0ab85b346a9
Bünyamin Erdoğan <bunyamin.erdogan@ogr.ksbu.edu.tr>
2024-12-28 22:03:28 +0300

    silme işlemleri

--- yazim_denetimi.py
+++
@@ -1,6 +0,0 @@
-from textblob import TextBlob
-
-def check_spelling(text):
-    blob = TextBlob(text)
-    corrected_text = blob.correct()  # Yazım hatalarını düzeltiyor
-    return str(corrected_text)
